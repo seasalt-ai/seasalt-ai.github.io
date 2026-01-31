29a922dbef56d806e1fced8243d3f779ba8e0b80
Xuchen Yao <yaoxuchen@gmail.com>
2026-01-31 00:47:08 +0000

    deploy(prod): from new-website 2026-01-31 00:47:05 UTC (backup: [0;34m➤ Creating backup tag: prod-backup-20260131-004501[0m [0;32m✔ Backup tag created: prod-backup-20260131-004501[0m prod-backup-20260131-004501)

---
+++ _astro/urls.D3xWLezf.js
@@ -0,0 +1,1 @@
+const e="https://meetings.hubspot.com/seasalt-ai/seasalt-meeting/",s="https://calendar.app.google/3w9kgPpkjHKqgJsN7",n=t=>["zh-cn","zh-tw"].includes(t.toLowerCase())?s:e;export{n as g};
