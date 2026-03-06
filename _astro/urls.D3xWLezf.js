a72494fe247fc9cd81a236ff90e99ed10e9ea2d6
Xuchen Yao <yaoxuchen@gmail.com>
2026-03-06 07:15:41 +0000

    deploy

--- _astro/urls.D3xWLezf.js
+++
@@ -1,1 +0,0 @@
-const e="https://meetings.hubspot.com/seasalt-ai/seasalt-meeting/",s="https://calendar.app.google/3w9kgPpkjHKqgJsN7",n=t=>["zh-cn","zh-tw"].includes(t.toLowerCase())?s:e;export{n as g};
---
+++ _astro/urls.DXIGcMxv.js
@@ -0,0 +1,1 @@
+const e="https://calendar.app.google/FLjCxzbYLqG6CkNs9/",n="https://calendar.app.google/3w9kgPpkjHKqgJsN7",o=t=>["zh-cn","zh-tw"].includes(t.toLowerCase())?n:e;export{o as g};
