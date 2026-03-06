e8877d1d2efe0c60c24bc4cb509d529c66e2a9c7
Xuchen Yao <yaoxuchen@gmail.com>
2026-03-06 18:22:47 +0000

    deploy(prod): from new-website 2026-03-06 18:22:46 UTC (backup: [0;34m➤ Creating backup tag: prod-backup-20260306-182048[0m [0;32m✔ Backup tag created: prod-backup-20260306-182048[0m prod-backup-20260306-182048)

---
+++ _astro/urls.BCRxJv15.js
@@ -0,0 +1,1 @@
+const e="https://calendar.app.google/GcdRQv1DVoiDaoCT6/",o="https://calendar.app.google/3w9kgPpkjHKqgJsN7",n=t=>["zh-cn","zh-tw"].includes(t.toLowerCase())?o:e;export{n as g};
--- _astro/urls.DXIGcMxv.js
+++
@@ -1,1 +0,0 @@
-const e="https://calendar.app.google/FLjCxzbYLqG6CkNs9/",n="https://calendar.app.google/3w9kgPpkjHKqgJsN7",o=t=>["zh-cn","zh-tw"].includes(t.toLowerCase())?n:e;export{o as g};
